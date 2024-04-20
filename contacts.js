f7f911d799706880c532af19a9ede00225918094
ex1kenny <ex1kenny4543@gmail.com>
2024-04-20 18:11:49 +0300

    home-fn1

--- contacts.js
+++ contacts.js
@@ -4,48 +4,6 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import shortid from "shortid";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const contactsPath = path.resolve(__dirname, "contacts.json");
-
-async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    return JSON.parse(data);
-  } catch (err) {
-    throw err;
-  }
-}
-
-async function getContactById(contactId) {
-  try {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    const contacts = JSON.parse(data);
-    return contacts.find((contact) => contact.id === contactId);
-  } catch (err) {
-    throw err;
-  }
-}
-
-async function removeContact(contactId) {
-  try {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    const contacts = JSON.parse(data);
-    const removedIndex = contacts.findIndex(
-      (contact) => contact.id === contactId
-    );
-    if (removedIndex === -1) {
-      return null;
-    }
-    const removedContact = contacts.splice(removedIndex, 1)[0];
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return removedContact;
-  } catch (err) {
-    throw err;
-  }
-}
-
 const randomID = () => {
   let id = shortid.generate();
   const extraChars = 20 - id.length;
@@ -55,13 +13,52 @@ const randomID = () => {
   return id + randomChars;
 };
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const contactsPath = path.resolve(__dirname, "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
+
+async function getContacts() {
+  const contacts = await listContacts();
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await getContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  if (typeof contact === "undefined") {
+    return null;
+  }
+  return contact;
+}
+async function removeContact(contactId) {
+  try {
+    const contacts = await getContacts();
+    const removedIndex = contacts.findIndex(
+      (contact) => contact.id === contactId
+    );
+    if (removedIndex === -1) {
+      return null;
+    }
+    const removedContact = contacts.splice(removedIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, 2));
+    return removedContact;
+  } catch (err) {
+    throw err;
+  }
+}
+
 async function addContact(name, email, phone) {
   try {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    const contacts = JSON.parse(data);
+    const contacts = await getContacts();
     const addContacts = { id: randomID(), name, email, phone };
     const newContacts = [...contacts, addContacts];
-    await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts, 2));
     return addContacts;
   } catch (err) {
     throw err;
